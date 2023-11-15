e400def7d8041ff6337a0feb55d6939f131c48ef
Gui-rosales <guilhermelexandre456@gmail.com>
2023-11-15 10:41:59 -0400

    refactor: small changes in repository interfaces

--- src/repositories/checkIns-repository.ts
+++ src/repositories/checkIns-repository.ts
@@ -4,4 +4,7 @@ export interface CheckInsRepository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
   findManyByUserId(userId: string, page: number): Promise<CheckIn[] | null>;
+  countByUserId(userId: string): Promise<number | null>;
+  findById(checkInId: string): Promise<CheckIn | null>;
+  save(checkIn: CheckIn): Promise<CheckIn>;
 }
